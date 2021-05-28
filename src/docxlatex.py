fef66c8f5c6a87e3fd3e341bb5d1341c426e156a
Hrushikesh <hrushikeshvaidya2002@gmail.com>
2021-05-28 16:18:50 +0530

    Add support for the sigma function

--- src/docxlatex.py
+++ src/docxlatex.py
@@ -1,7 +1,7 @@
 import zipfile
 from defusedxml import ElementTree
 
-from tagparsers import tag_to_latex
+from tagparsers import tag_to_latex, qn
 
 ns_map = {
     'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
@@ -9,66 +9,64 @@ ns_map = {
 }
 
 
-def qn(tag):
-    """
-    A utility function to turn a namespace
-    prefixed tag name into a Clark-notation qualified tag name for lxml. For
-    example, qualified_name('p:cSld') returns '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}cSld'
-    Source: https://github.com/python-openxml/python-docx/
+class Document:
+    def __init__(self, document, inline_delimiter='$', block_delimiter='$$'):
+        self.document = document
+        self.inline_delimiter = inline_delimiter
+        self.block_delimiter = block_delimiter
     
-    :param tag:str - A namespace-prefixed tag name
-    :return qn:str - A Clark-notation qualified name tag for lxml.
-    """
-    prefix, tag_root = tag.split(':')
-    uri = ns_map[prefix]
-    return '{{{}}}{}'.format(uri, tag_root)
-
-
-def xml_to_text(xml):
-    """
-    Recursively iterate over the ElementTree of the word document and extract text content from supported tags.
-    
-    :param xml:str - XML string to be parsed into an xml.etree.Element object.
-    :return text:str - The text contained in the tag
-    """
-    text = ''
-    root = ElementTree.fromstring(xml)
-    for child in root.iter():
-        if child.get('skip_iteration', False):
-            continue
+    def get_text(self, get_header_text=False, get_footer_text=False):
+        """
+        Extract the text from the .docx file while converting the equations in the document
+        to valid LaTeX syntax, enclosed within the specified delimiters
         
-        if child.tag == qn('w:t'):
-            text += child.text if child.text is not None else ''
-        elif child.tag == qn('m:nary'):
-            text += tag_to_latex(child)
-        elif child.tag == qn('m:r'):
-            text += ''.join(child.itertext())
+        :param get_header_text:bool - True if you want to extract text from the header, False otherwise
+        :param get_footer_text:bool - True if you want to extract text from the footer, False otherwise
+        :return text:str - The extracted text
+        """
+        zip_f = zipfile.ZipFile(self.document)
+        text = ''
+        for f in zip_f.namelist():
+            if get_header_text and f.startswith('word/header'):
+                text += self.xml_to_text(zip_f.read(f))
+            if f.startswith('word/document'):
+                text += self.xml_to_text(zip_f.read(f))
+            if get_footer_text and f.startswith('word/footer'):
+                text += self.xml_to_text(zip_f.read(f))
         
-        elif child.tag == qn('w:tab'):
-            text += '\t'
-        elif child.tag == qn('w:br') or child.tag == qn('w:cr'):
-            text += '\n'
-        elif child.tag == qn('w:p'):
-            text += '\n\n'
-    
-    return text
+        zip_f.close()
+        return text
 
+    def xml_to_text(self, xml):
+        """
+            Recursively iterate over the ElementTree of the word document and extract text content from supported tags.
 
-def get_text(doc_path):
-    """
+            :param xml:str - XML string to be parsed into an xml.etree.Element object.
+            :return text:str - The text contained in the tag
+
+            TODO -
+                1. Check for newlines before and after equation and switch to block_delimiter accordingly
+            """
+        text = ''
+        root = ElementTree.fromstring(xml)
+        for child in root.iter():
+            if child.get('docxlatex_skip_iteration', False):
+                continue
+        
+            if child.tag == qn('w:t'):
+                text += child.text if child.text is not None else ''
+            elif child.tag == qn('m:nary'):
+                text += self.inline_delimiter + ' '
+                text += tag_to_latex(child)
+                text += ' ' + self.inline_delimiter
+            elif child.tag == qn('m:r'):
+                text += ''.join(child.itertext())
+        
+            elif child.tag == qn('w:tab'):
+                text += '\t'
+            elif child.tag == qn('w:br') or child.tag == qn('w:cr'):
+                text += '\n'
+            elif child.tag == qn('w:p'):
+                text += '\n\n'
     
-    The whole point of this library. Takes in the path to the .docx document and
-    extracts text from it. For now, requires all the equations in the document to be converted to linear.
-    
-    :param doc_path:str - The full path to the word document to be read
-    :return text:str - The extracted text from the document (this is where you get the latex)
-    
-    TODOS-
-        1. Clean up some equations that convert into linear but do not give valid LaTeX.
-    """
-    zip_f = zipfile.ZipFile(doc_path)
-    text = ''
-    for f in zip_f.namelist():
-        if f.startswith('word/document') or f.startswith('word/header') or f.startswith('word/footer'):
-            text += xml_to_text(zip_f.read(f))
-    return text
+        return text
--- src/tagparsers.py
+++ src/tagparsers.py
@@ -1,7 +1,61 @@
 """
-
+    Utility functions to extract text from different mathematical equations and
+    convert them into LaTeX
 """
 
 
+ns_map = {
+    'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
+    'm': 'http://schemas.openxmlformats.org/officeDocument/2006/math',
+}
+
+
 def tag_to_latex(tag):
-    return
+    text = ''
+    exp = ''
+    for child in tag.iter():
+        if child.tag == qn('m:chr'):
+            exp = child.get('{http://schemas.openxmlformats.org/officeDocument/2006/math}val')
+    text += supported_exps[exp](tag)
+    return text
+
+
+def sigma(tag):
+    """
+    Constructs LaTeX of the form \\sum_{}^{}{}
+    :return latex:str - A sigma expression
+    """
+    latex = '\\sum_{{{}}}^{{{}}}{{{}}}'
+    blocks = ['', '', '']
+    curr_block = 0
+    for child in tag.iter():
+        child.set('docxlatex_skip_iteration', True)
+        if child.tag == qn('m:sub'):
+            curr_block = 0
+        elif child.tag == qn('m:sup'):
+            curr_block = 1
+        elif child.tag == qn('m:e'):
+            curr_block = 2
+        blocks[curr_block] += child.text if child.text is not None else ''
+    
+    return latex.format(blocks[0], blocks[1], blocks[2])
+
+
+def qn(tag):
+    """
+    A utility function to turn a namespace
+    prefixed tag name into a Clark-notation qualified tag name for lxml. For
+    example, qualified_name('p:cSld') returns '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}cSld'
+    Source: https://github.com/python-openxml/python-docx/
+
+    :param tag:str - A namespace-prefixed tag name
+    :return qn:str - A Clark-notation qualified name tag for lxml.
+    """
+    prefix, tag_root = tag.split(':')
+    uri = ns_map[prefix]
+    return '{{{}}}{}'.format(uri, tag_root)
+
+
+supported_exps = {
+    '∑': sigma,
+}
