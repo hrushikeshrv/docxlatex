00c29a697c881094fbece30557c04915c26f22c3
Hrushikesh <hrushikeshvaidya2002@gmail.com>
2021-06-14 08:51:06 +0530

    Add support for extracting media

--- docxlatex/docxlatex.py
+++ docxlatex/docxlatex.py
@@ -1,27 +1,30 @@
 import zipfile
 from defusedxml import ElementTree
 import re
+import os
 
 from .tagparsers import tag_to_latex, qn
 
 
 class Document:
-    def __init__(self, document, inline_delimiter='$', block_delimiter='$$', get_header=False, get_footer=False):
+    def __init__(self, document, inline_delimiter='$', block_delimiter='$$'):
         self.document = document
         self.inline_delimiter = inline_delimiter
         self.block_delimiter = block_delimiter
-        self.get_header_text = get_header
-        self.get_footer_text = get_footer
     
-    def get_text(self, get_header_text=False, get_footer_text=False):
+    def get_text(self, get_header_text=False, get_footer_text=False, image_dir=None, extensions=None):
         """
         Extract the text from the .docx file while converting the equations in the document
         to valid LaTeX syntax, enclosed within the specified delimiters
         
         :param get_header_text:bool - True if you want to extract text from the header, False otherwise
         :param get_footer_text:bool - True if you want to extract text from the footer, False otherwise
+        :param image_dir:str - The path to the directory where you want to store images
+        :param extensions:List[str], tuple - A list of tuple of string of the extensions you want to extract (['.jpg', '.png'])
         :return text:str - The extracted text
         """
+        if extensions is None:
+            extensions = ['.jpg', '.jpeg', '.png', '.svg', '.bmp', '.gif']
         zip_f = zipfile.ZipFile(self.document)
         text = ''
         for f in zip_f.namelist():
@@ -32,6 +35,13 @@ class Document:
             if get_footer_text and f.startswith('word/footer'):
                 text += self.xml_to_text(zip_f.read(f))
         
+        if image_dir is not None:
+            for f in zip_f.namelist():
+                _, extension = os.path.splitext(f)
+                if extension in extensions:
+                    destination = os.path.join(image_dir, os.path.basename(f))
+                    with open(destination, 'wb') as destination_file:
+                        destination_file.write(zip_f.read(f))
         zip_f.close()
         return text
 
@@ -43,6 +53,7 @@ class Document:
             :return text:str - The text contained in the tag
             """
         text = ''
+        n_images = 0
         root = ElementTree.fromstring(xml)
         for child in root.iter():
             if child.get('docxlatex_skip_iteration', False):
@@ -56,6 +67,9 @@ class Document:
                 text += ' ' + self.inline_delimiter
             elif child.tag == qn('m:r'):
                 text += ''.join(child.itertext())
+            elif child.tag == qn('w:drawing'):
+                n_images += 1
+                text += f'\nIMAGE#{n_images}-image{n_images}\n'
         
             elif child.tag == qn('w:tab'):
                 text += '\t'
