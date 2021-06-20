bbf7ab04d5ee17364d05c7cb0fa195e32a9e5b19
Hrushikesh <hrushikeshvaidya2002@gmail.com>
2021-06-20 09:11:07 +0530

    Bump version 0.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (ROOT / "README.md").read_text()
 
 setup(
     name='docxlatex',
-    version='0.1.5',
+    version='0.1.6',
     description='Extract text from .docx files with support for inserted equations',
     long_description=README,
     long_description_content_type='text/markdown',
