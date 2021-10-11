16c368dad2d7ff7d7bce6d0f2ac7063cefa57c0b
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2021-10-11 12:45:00 +0530

    Remove redundant function defs before building new architecture

--- docxlatex/tagparsers.py
+++ docxlatex/tagparsers.py
@@ -43,46 +43,6 @@ def linear_expression(tag):
     return text
 
 
-# def sigma(tag):
-#     """
-#     Constructs LaTeX of the form \\sum_{}^{}{}
-#     :return latex:str - A sigma expression
-#     """
-#     latex = '\\sum_{{{}}}^{{{}}}{{{}}}'
-#     blocks = ['', '', '']
-#     curr_block = 0
-#     for child in tag.iter():
-#         child.set('docxlatex_skip_iteration', True)
-#         if child.tag == qn('m:sub'):
-#             curr_block = 0
-#         elif child.tag == qn('m:sup'):
-#             curr_block = 1
-#         elif child.tag == qn('m:e'):
-#             curr_block = 2
-#         blocks[curr_block] += child.text if child.text is not None else ''
-#
-#     return latex.format(blocks[0], blocks[1], blocks[2])
-#
-#
-# def frac(tag):
-#     """
-#     Constructs LaTeX of the form \\frac{}{}
-#     :param tag:
-#     :return latex:str - A latex fraction
-#     """
-#     latex = '\\frac{{{}}}{{{}}}'
-#     blocks = ['', '']
-#     curr_block = 0
-#     for child in tag.iter():
-#         child.set('docxlatex_skip_iteration', True)
-#         if child.tag == qn('m:num'):
-#             curr_block = 0
-#         elif child.tag == qn('m:den'):
-#             curr_block = 1
-#         blocks[curr_block] += child.text if child.text is not None else ''
-#     return latex.format(blocks[0], blocks[1])
-
-
 def qn(tag):
     """
     A utility function to turn a namespace
@@ -95,9 +55,3 @@ def qn(tag):
     prefix, tag_root = tag.split(':')
     uri = ns_map[prefix]
     return '{{{}}}{}'.format(uri, tag_root)
-
-#
-# supported_exps = {
-#     '∑': sigma,
-#     'frac': frac,
-# }
