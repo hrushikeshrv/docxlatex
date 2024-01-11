efd0cddcf042b8fe74ad7691e1bd88358ab5b1a7
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2024-01-11 10:26:19 +0530

    Blackened source files

--- docxlatex/ommlparser.py
+++ docxlatex/ommlparser.py
@@ -3,16 +3,17 @@ class Equation:
     Wrapper class representing the equation being extracted. Responsible for
     parsing OMML and building an Equation object
     """
+
     def __init__(self):
         self.components = []
 
     def add_component(self, component):
         self.components.append(component)
-    
+
     def to_latex(self):
-        latex = ''
+        latex = ""
         for c in self.components:
-            latex += c.to_latex() + ' '
+            latex += c.to_latex() + " "
         return latex.strip()
 
 
@@ -20,17 +21,18 @@ class Block:
     """
     Represents a block for a component in the equation
     """
+
     def __init__(self, parent):
         self.children = []
         self.parent = parent
-    
+
     def add_child(self, component):
         self.children.append(component)
-    
+
     def to_latex(self):
         if len(self.children) == 0:
-            return ''
-        latex = ''
+            return ""
+        latex = ""
         for c in self.children:
             if isinstance(c, str):
                 latex += c
@@ -43,11 +45,14 @@ class Component:
     """
     Represents a component. Abstract base class to inherit from
     """
+
     def __init__(self, blocks=None, parent=None):
         if blocks is None:
             blocks = []
         self.blocks = blocks
         self.parent = parent
-    
+
     def to_latex(self):
-        raise NotImplementedError('This method is to be overridden in all child classes inheriting from Component')
+        raise NotImplementedError(
+            "This method is to be overridden in all child classes inheriting from Component"
+        )
