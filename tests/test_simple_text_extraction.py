aab5e32c1f3e35926df6b8916659b0dd9a42ad92
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2024-07-17 16:06:43 +0530

    Added simple text extraction test case

--- tests/test_simple_text_extraction.py
+++ tests/test_simple_text_extraction.py
@@ -4,10 +4,16 @@ from docxlatex import Document
 
 
 class SimpleTextExtractionTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_simple_text_extraction(self):
         doc = Document('./docx/simple-text.docx')
         text = doc.get_text().strip()
         expected = "Test Document\n\nThis is paragraph 1."
+        self.assertEqual(text, expected)
+
+    def test_simple_equation_extraction_linear_format(self):
+        doc = Document('./docx/simple-equation-linear-format.docx')
+        text = doc.get_text().strip()
+        expected = '$ a^2+b^2=c^2 $'
         self.assertEqual(text, expected)
 
 
