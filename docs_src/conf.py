c1bf3633967e48ce851c798e27a3d96531b4f72b
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2025-05-29 13:37:20 +0530

    Add google analytics

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -18,7 +18,10 @@ release = 'v1.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinxcontrib.googleanalytics',
+]
+googleanalytics_id = 'G-TGM5F6WJNV'
 
 templates_path = ['_templates']
 exclude_patterns = []
