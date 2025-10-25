dff7475a7290366ecc9668137fad805064cc5355
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2025-10-24 21:56:02 -0700

    Bump patch version

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -13,7 +13,7 @@
 project = "docxlatex"
 copyright = "2025, Hrushikesh Vaidya (@hrushikeshrv)"
 author = "Hrushikesh Vaidya (@hrushikeshrv)"
-release = "v1.2.1"
+release = "v1.2.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
